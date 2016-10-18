2b681467ec105c6120534af01364754ded4c5253
Hugo Holmqvist <hugo.holm@live.com>
2016-10-18 13:42:50 +0300

    Fix board filter param

--- src/main/java/fi/tekislauta/db/objects/BoardDao.java
+++ src/main/java/fi/tekislauta/db/objects/BoardDao.java
@@ -20,27 +20,27 @@ public class BoardDao implements DatabaseObject {
     public BoardDao() {}
 
     @Override
-    public Object fetch(Database db, String abbreviation) throws SQLException {
-        PreparedStatement statement = db.getConnection().prepareStatement("SELECT * FROM Board WHERE name= ?");
-        statement.setString(1, abbreviation);
+    public Object fetch(Database db, String id) throws SQLException {
+        PreparedStatement statement = db.getConnection().prepareStatement("SELECT * FROM Board WHERE id= ?");
+        statement.setInt(1, Integer.parseInt(id));
 
         ResultSet rs = statement.executeQuery();
         Board b = new Board();
         if (!rs.next()) {
-            b.setError("Cannot find board " + abbreviation + " :(");
+            b.setError("Cannot find board with id " + id + " :(");
             return b;
         }
 
         b.setId(rs.getInt(1));
         b.setName(rs.getString(2));
-        b.setAbbreviation(abbreviation);
+        b.setAbbreviation(rs.getString("abbreviation"));
         b.setDescription(rs.getString(4));
 
         return b;
     }
 
     @Override
-    public List<Object> fetchAll(Database db) throws SQLException {
+    public List<Object> fetchAll(Database db, String filter) throws SQLException {
         PreparedStatement statement = db.getConnection().prepareStatement("SELECT * FROM Board");
 
         ResultSet rs = statement.executeQuery();
