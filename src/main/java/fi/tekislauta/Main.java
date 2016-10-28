6987990e61370fdd5968202df036ce378e0233ad
MergHQ <hugo.holm@live.com>
2016-10-28 18:34:42 +0300

    Fix more things for heroku

--- src/main/java/fi/tekislauta/Main.java
+++ src/main/java/fi/tekislauta/Main.java
@@ -14,6 +14,6 @@ public class Main {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        return 1234; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
